5bca7164c29b56b37753b6be4ea7ae5d4a3f971d
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-07-31 15:53:36 +0300

    Fix 1

--- src/QueryDataReader.php
+++ src/QueryDataReader.php
@@ -5,7 +5,12 @@ declare(strict_types=1);
 namespace Yiisoft\Data\Db;
 
 /**
- * Base class for QueryDataReaderInterface
+ * Base class for `QueryDataReaderInterface`
+ *
+ * @template TKey as array-key
+ * @template TValue as array|object
+ *
+ * @extends AbstractQueryDataReader<TKey, TValue>
  */
 final class QueryDataReader extends AbstractQueryDataReader
 {
