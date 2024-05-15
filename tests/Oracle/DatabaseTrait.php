c563f98f4cbfd5a65d97be7ba20234db1fa709c9
Alexey Rogachev <arogachev90@gmail.com>
2024-05-15 14:34:13 +0500

    Add oracle tests

---
+++ tests/Oracle/DatabaseTrait.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Data\Db\Tests\Oracle;
+
+use PDO;
+use Yiisoft\Cache\ArrayCache;
+use Yiisoft\Db\Cache\SchemaCache;
+use Yiisoft\Db\Driver\Pdo\PdoConnectionInterface;
+use Yiisoft\Db\Oracle\Connection;
+use Yiisoft\Db\Oracle\Driver;
+
+trait DatabaseTrait
+{
+    protected function getConnection(): PdoConnectionInterface
+    {
+        $database = getenv('YII_ORACLE_DATABASE');
+        $host = getenv('YII_ORACLE_HOST');
+        $port = (int) getenv('YII_ORACLE_PORT');
+        $user = getenv('YII_ORACLE_USER');
+        $password = getenv('YII_ORACLE_PASSWORD');
+
+        $pdoDriver = new Driver("oci:dbname=$host/XE;", $user, $password);
+        $pdoDriver->charset('AL32UTF8');
+        $pdoDriver->attributes([PDO::ATTR_STRINGIFY_FETCHES => true]);
+
+        return new Connection($pdoDriver, new SchemaCache(new ArrayCache()));
+    }
+}
