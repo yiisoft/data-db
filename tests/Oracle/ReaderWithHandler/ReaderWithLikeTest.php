c030b3a414236a5fe475a3093825a16e7f33fed8
Alexey Rogachev <arogachev90@gmail.com>
2024-10-17 19:19:19 +0500

    Fix

--- tests/Oracle/ReaderWithHandler/ReaderWithLikeTest.php
+++ tests/Oracle/ReaderWithHandler/ReaderWithLikeTest.php
@@ -10,4 +10,18 @@ use Yiisoft\Data\Db\Tests\Oracle\DatabaseTrait;
 final class ReaderWithLikeTest extends BaseReaderWithLikeTestCase
 {
     use DatabaseTrait;
+
+    public static function dataWithReader(): array
+    {
+        $data = parent::dataWithReader();
+        $data['search: contains, same case, case sensitive: true'] = ['email', 'ed@be', true, [2]];
+        $data['search: contains, different case, case sensitive: true'] = ['email', 'SEED@', true, []];
+        // TODO: Remove after changes in yiisoft/db
+        unset(
+            $data['search: contains, different case, case sensitive: null'],
+            $data['search: contains, different case, case sensitive: false'],
+        );
+
+        return $data;
+    }
 }
