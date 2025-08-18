647183972eb8cf2d50b5dea5b34e3cc6f193ff86
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-08-18 22:16:41 +0300

    Throw exception when count is string (#100)

---
+++ tests/Sqlite/QueryDataReader/StringCount/QueryStub.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Data\Db\Tests\Sqlite\QueryDataReader\StringCount;
+
+use Yiisoft\Db\Query\Query;
+
+final class QueryStub extends Query
+{
+    public function __construct()
+    {
+    }
+
+    public function count(string $sql = '*'): int|string
+    {
+        return '9223372036854775808';
+    }
+}
