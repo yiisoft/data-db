dcf67cf5880bdb99160611395cf925cdaf2339b6
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-08-26 11:34:30 +0300

    test 7

--- tests/Sqlite/QueryDataReaderTest.php
+++ tests/Sqlite/QueryDataReaderTest.php
@@ -18,6 +18,7 @@ use Yiisoft\Data\Reader\Filter\All;
 use Yiisoft\Data\Reader\Filter\AndX;
 use Yiisoft\Data\Reader\Filter\Equals;
 use Yiisoft\Data\Reader\Filter\None;
+use Yiisoft\Data\Reader\Sort;
 use Yiisoft\Test\Support\Log\SimpleLogger;
 
 final class QueryDataReaderTest extends TestCase
@@ -184,5 +185,55 @@ final class QueryDataReaderTest extends TestCase
         $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage('$batchSize cannot be less than 1.');
         $dataReader->withBatchSize(0);
+    }
+
+    public function testGetSort(): void
+    {
+        $dataReader = new QueryDataReader(
+            TestHelper::createSqliteConnection()->createQuery(),
+        );
+
+        $this->assertNull($dataReader->getSort());
+
+        $sort = Sort::any();
+        $dataReader = $dataReader->withSort($sort);
+        $this->assertSame($sort, $dataReader->getSort());
+    }
+
+    public function testGetFilter(): void
+    {
+        $dataReader = new QueryDataReader(
+            TestHelper::createSqliteConnection()->createQuery(),
+        );
+
+        $this->assertInstanceOf(All::class, $dataReader->getFilter());
+
+        $filter = new None();
+        $dataReader = $dataReader->withFilter($filter);
+        $this->assertSame($filter, $dataReader->getFilter());
+    }
+
+    public function testGetLimit(): void
+    {
+        $dataReader = new QueryDataReader(
+            TestHelper::createSqliteConnection()->createQuery(),
+        );
+
+        $this->assertNull($dataReader->getLimit());
+
+        $dataReader = $dataReader->withLimit(10);
+        $this->assertSame(10, $dataReader->getLimit());
+    }
+
+    public function testGetOffset(): void
+    {
+        $dataReader = new QueryDataReader(
+            TestHelper::createSqliteConnection()->createQuery(),
+        );
+
+        $this->assertSame(0, $dataReader->getOffset());
+
+        $dataReader = $dataReader->withOffset(10);
+        $this->assertSame(10, $dataReader->getOffset());
     }
 }
