201ccf02452204ff24c0ce851e463816ce1ccfba
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-08-27 13:37:31 +0300

    Update LIKE tests (#106)

--- tests/Mysql/ReaderWithFilter/ReaderWithLikeTest.php
+++ tests/Mysql/ReaderWithFilter/ReaderWithLikeTest.php
@@ -10,14 +10,4 @@ use Yiisoft\Data\Db\Tests\Mysql\DatabaseTrait;
 final class ReaderWithLikeTest extends BaseReaderWithLikeTestCase
 {
     use DatabaseTrait;
-
-    public static function dataWithReader(): array
-    {
-        $data = parent::dataWithReader();
-        $data['search: contains, same case, case sensitive: true'] = ['email', 'ed@be', true, [2]];
-        // TODO: Add after changes in yiisoft/db
-        // $data['search: contains, different case, case sensitive: true'] = ['email', 'SEED@', true, []];
-
-        return $data;
-    }
 }
