b561be0ef126b38dbf5d0e77de1e4679d21468e9
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-09-04 20:01:34 +0300

    phpdoc

--- src/FieldMapper/FieldMapperInterface.php
+++ src/FieldMapper/FieldMapperInterface.php
@@ -6,7 +6,17 @@ namespace Yiisoft\Data\Db\FieldMapper;
 
 use Yiisoft\Db\Expression\ExpressionInterface;
 
+/**
+ * Maps field names to database column names or expressions.
+ */
 interface FieldMapperInterface
 {
+    /**
+     * Maps a field name to a database column name or expression.
+     *
+     * @param string $field The field name to map.
+     *
+     * @return string|ExpressionInterface The mapped column name or expression.
+     */
     public function map(string $field): string|ExpressionInterface;
 }
