8f0f969a61b85d8ea971deb008d932bce7960079
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-11-19 17:48:53 +0300

    Cleanup tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,10 +6,3 @@ if (getenv('ENVIRONMENT', local_only: true) !== 'ci') {
     $dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
     $dotenv->load();
 }
-
-/**
- * @link https://github.com/krakjoe/uopz/issues/172
- */
-if (function_exists('uopz_allow_exit')) {
-    uopz_allow_exit(true);
-}
